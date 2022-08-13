4f0824874fc59a983fa5d8c32728258d861a437f
danielyovchev <daniel.yovchev@gmail.com>
2022-08-13 15:29:57 +0300

    feign edit

--- carApiProject/api/src/main/java/com/example/api/feign/ApiFeignClient.java
+++ carApiProject/api/src/main/java/com/example/api/feign/ApiFeignClient.java
@@ -1,11 +1,12 @@
 package com.example.api.feign;
 
+import com.example.api.model.CarApiResponseModel;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "apiProjectFeign", url = "http://localhost:8082")
+@FeignClient(name = "apiProjectFeign", url = "http://localhost:8083")
 public interface ApiFeignClient {
-    @GetMapping("/createCar")
-    String getVin(@RequestParam String vin);
+    @GetMapping("/carInfo")
+    CarApiResponseModel getCar(@RequestParam String vin);
 }
